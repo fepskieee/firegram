57a9a6cdc162afbb61956fe3e02bcef3e6db74f2
feps <120133472+fepskieee@users.noreply.github.com>
2023-10-18 15:02:32 +0800

    feat(gallery): Improved top navigation bar UI.

--- src/components/react/Navbar.jsx
+++ src/components/react/Navbar.jsx
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 
 const Navbar = ({brandName}) => {
   return (
-    <nav className="flex w-screen lg:max-w-screen-lg my-12 mx-auto border">
+    <nav className="flex justify-center w-full md:justify-between lg:max-w-screen-lg my-12 mx-auto">
       <a href="#"
         className="ml-4 lg:ml-0"
       >
         <span className="text-xl text-pink-500">{brandName}</span>
-        </a>
+      </a>
     </nav>
   )
 }
