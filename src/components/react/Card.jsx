e339cb7d7293540e962e99310fc0a094ab73de2e
feps <120133472+fepskieee@users.noreply.github.com>
2023-10-20 09:31:26 +0800

    feat(card): Improved card component UI.

--- src/components/react/Card.jsx
+++ src/components/react/Card.jsx
@@ -1,15 +1,14 @@
 import PropTypes from 'prop-types'
 
 const Card = () => {
-  const arr = [1,2,3];
 
   return (
     <li>
       <img
         src="https://images.unsplash.com/photo-1519378058457-4c29a0a2efac?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1304"
-        className="max-h-80 w-full mx-auto sm:max-w-screen-sm object-cover shadow-md shadow-slate-700"
-        width="500"
-        height="500"
+        className="min-w-64 min-h-64 object-cover shadow-md"
+        width="1080"
+        height="1080"
       />
     </li>
   )
