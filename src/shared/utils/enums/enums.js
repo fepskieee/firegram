3bda505d12f04767c76bf6de2e049bfa6dabd6f9
feps <120133472+fepskieee@users.noreply.github.com>
2023-10-26 12:51:30 +0800

    feat(enums): Added image types enums.

--- src/shared/utils/enums/enums.js
+++ src/shared/utils/enums/enums.js
@@ -3,3 +3,8 @@ const ON_GOING = "on-going";
 const DONE = "done";
 const ERROR = "error";
 export const STATUS = [PENDING, ON_GOING, DONE, ERROR];
+
+const PNG = "image/png"
+const JPG = "image/jpeg"
+export const IMAGE_TYPES = [PNG, JPG];
+export const ERROR_MSG_IMAGE_TYPE = "Please select an image file only (png or jpg)"
