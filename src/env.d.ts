79b4903a753df5efb40ba571c98b596597a53ac8
feps <120133472+fepskieee@users.noreply.github.com>
2023-12-21 15:29:23 +0800

    Revert "feature/gallery"

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,14 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly FIREBASE_API_KEY: string;
-  readonly FIREBASE_AUTH_DOMAIN: string;
-  readonly FIREBASE_PROJECT_ID: string;
-  readonly FIREBASE_STORAGE_BUCKET: string;
-  readonly FIREBASE_MESSAGING_Sender_ID: string;
-  readonly FIREBASE_APP_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
